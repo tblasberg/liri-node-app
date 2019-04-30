9707af9c232059ebc5903b82f21dc5497ce8c67b
tblasberg <terena.blasberg@gmail.com>
2019-04-30 00:03:27 -0400

    Spotify, OMDB and Bands In Town - Done. Pending item 4.

--- keys.js
+++ keys.js
@@ -1,8 +1,14 @@
-console.log('this is loaded');
+// console.log('this is loaded');
 SPOTIFY_ID="032c3beda97b4f688e4ece024969c56e";
 SPOTIFY_SECRET="11ab0c7743004a3192fd32810fedc866";
+bitAPI = "88d400d4-397f-448a-a027-f72900e7f80f";
+
 
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+exports.bandsintown = {
+  
+}
--- liri.js
+++ liri.js
@@ -3,100 +3,38 @@ var Spotify = require('node-spotify-api');
 var keys = require("./keys");
 var spotify = new Spotify(keys.spotify);
 var axios = require("axios");
+var moment = require("moment");
 
-//-------------------------------------------------
-//SPOTIFY
 
 let userInput = process.argv.slice(3).join("+");
-// console.log("User Input: "+ userInput);
+console.log("User Input: " + userInput);
 
 
-if(process.argv[2] === "spotify-this-song" && process.argv[3] === undefined){
+if (process.argv[2] === "spotify-this-song" && process.argv[3] === undefined) {
+    console.log("hi¨")
     spotify
-    .search({ type: 'track', query: "the+sign" })
-    .then(function(data) {
+    .search({
+        type: 'track',
+        query: "the+sign"
+    })
+    .then(function (data) {
         console.log("Artist: " + data.tracks.items[3].artists[0].name);
         console.log("Song: " + data.tracks.items[3].name);
         console.log("Link to the song: " + data.tracks.items[3].external_urls.spotify);
         console.log("Album: " + data.tracks.items[3].album.name);
     })
-    .catch(function(err) {
+    .catch(function (err) {
         console.log("not working " + err);
     });
-}
-    else if (process.argv[2] === "spotify-this-song"){
-        music(userInput);
-    }  
-    else if(process.argv[2] === "movie-this"){
-        movie(userInput);
-    }
-    else if(process.argv[2] === "movie-this" && process.argv[3]=== undefined){
-        // userInput = "mr+nobody"
-        // console.log(userInput);
-        // movie();
-        axios.get("http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy").then(
-            function(response) {
-                console.log(response);
-                // console.log("Title of the movie: " + response.data.Title);
-                // console.log("Released: " + response.data.Released);
-                // console.log("IMDB Rating of the movie: " + response.data.imdbRating);
-                // console.log("Rotten Tomatoes Rating of the movie " + response.data.Ratings[2]);//Figure it out
-                // console.log("Country: " + response.data.Country);
-                // console.log("Language: " + response.data.Language);
-                // console.log("Plot of the movie: " + response.data.Plot);
-                // console.log("Actors: " + response.data.Actors);
-      }
-    ) .catch(function(err) {
-        console.log(err);
-        });
-    ;
-
-
-    }
-
-
-
-
-
-function music(){ 
-    spotify
-  .search({ type: 'track', query: userInput })
-  .then(function(data) {
-    //   console.log(data.tracks.items[0].artists[0]);
-    console.log("Artist: " + data.tracks.items[0].artists[0].name);
-    console.log("Song: " + data.tracks.items[0].name);
-    console.log("Link to the song: " + data.tracks.items[0].external_urls.spotify);
-    console.log("Album: " + data.tracks.items[0].album.name);
-  })
-  .catch(function(err) {
-    console.log(err);
-    });
-}
-
-
-/// Use switch
-//----------------------------------------------------------
-//BANDS IN TOWN
-
-// Need axios
-
-// 1. `node liri.js concert-this <artist/band name here>`
-
-//    * This will search the Bands in Town Artist Events API (`"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"`) for an artist and render the following information about each event to the terminal:
-
-//      * Name of the venue
-
-//      * Venue location
-
-//      * Date of the Event (use moment to format this as "MM/DD/YYYY")
-
-
-//----------------------------------------------------------
-//MOVIES 
-
-function movie(){
-    axios.get("http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy").then(
-        function(response) {
+} 
+else if (process.argv[2] === "spotify-this-song") {
+    console.log("h¨")
+    music(userInput);
+} 
+else if (process.argv[2] === "movie-this" && process.argv[3] === undefined) {
+    console.log("this condition is not running");
+    axios.get("http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy").then(
+        function (response) {
             // console.log(response);
             console.log("Title of the movie: " + response.data.Title);
             console.log("Released: " + response.data.Released);
@@ -106,10 +44,84 @@ function movie(){
             console.log("Language: " + response.data.Language);
             console.log("Plot of the movie: " + response.data.Plot);
             console.log("Actors: " + response.data.Actors);
-  }
-) .catch(function(err) {
-    console.log(err);
-    });
-;
+        }).catch(function (err) {
+            console.log(err);
+    });;
+} 
+else if (process.argv[2] === "movie-this") {
+    movie(userInput);
+}
+else if (process.argv[2] === "concert-this") {
+    bands();
 }
 
+//-------------------------------------------------
+//SPOTIFY
+
+function music() {
+    spotify
+        .search({
+            type: 'track',
+            query: userInput
+        })
+        .then(function (data) {
+            //   console.log(data.tracks.items[0].artists[0]);
+            console.log("Artist: " + data.tracks.items[0].artists[0].name);
+            console.log("Song: " + data.tracks.items[0].name);
+            console.log("Link to the song: " + data.tracks.items[0].external_urls.spotify);
+            console.log("Album: " + data.tracks.items[0].album.name);
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+}
+
+//----------------------------------------------------------
+//MOVIES 
+
+function movie() {
+    axios.get("http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy").then(
+        function (response) {
+            // console.log(response);
+            console.log("Title of the movie: " + response.data.Title);
+            console.log("Released: " + response.data.Released);
+            console.log("IMDB Rating of the movie: " + response.data.imdbRating);
+            console.log("Rotten Tomatoes Rating of the movie " + response.data.Ratings[2]); //Figure it out
+            console.log("Country: " + response.data.Country);
+            console.log("Language: " + response.data.Language);
+            console.log("Plot of the movie: " + response.data.Plot);
+            console.log("Actors: " + response.data.Actors);
+        }
+    ).catch(function (err) {
+        console.log(err);
+    });;
+}
+
+//----------------------------------------------------------
+//BANDS IN TOWN
+
+function bands(){
+    queryUrl = "https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=88d400d4-397f-448a-a027-f72900e7f80f"
+
+    console.log("queryUrl: " + queryUrl);
+
+    axios.get(queryUrl).then(function(response) {
+        for(var i =0; i<response.data.length; i++){
+            console.log("Name of the venue: " + response.data[i].venue.name);
+            console.log("Venue location: " + response.data[i].venue.city + ", " + response.data[i].venue.country);
+            console.log("Date of the Event: " +  moment(response.data[i].datetime).format("MM/DD/YYYY"))
+            console.log("-------------------------------");
+        }
+      })
+      .catch(function(err) {
+          console.log("not working: " + err)});
+}
+
+
+// 4. `node liri.js do-what-it-says`
+
+//    * Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
+
+//      * It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
+
+//      * Edit the text in random.txt to test out the feature for movie-this and concert-this.
