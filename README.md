475898240238a3ee1307bfb0c79078f51d3138b9
tblasberg <terena.blasberg@gmail.com>
2019-05-01 12:39:29 -0400

    Do-What-It-Says in progress

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# liri-node-app
+liri-node-app
+
--- liri.js
+++ liri.js
@@ -4,56 +4,36 @@ var keys = require("./keys");
 var spotify = new Spotify(keys.spotify);
 var axios = require("axios");
 var moment = require("moment");
+var fs = require("fs");
 
 
 let userInput = process.argv.slice(3).join("+");
 console.log("User Input: " + userInput);
+let randomInput; 
+let randomAction;
+
+console.log("Global randomInput: " + randomInput);
+console.log("Global randomAction: " + randomAction);
 
 
-if (process.argv[2] === "spotify-this-song" && process.argv[3] === undefined) {
-    console.log("hi¨")
-    spotify
-    .search({
-        type: 'track',
-        query: "the+sign"
-    })
-    .then(function (data) {
-        console.log("Artist: " + data.tracks.items[3].artists[0].name);
-        console.log("Song: " + data.tracks.items[3].name);
-        console.log("Link to the song: " + data.tracks.items[3].external_urls.spotify);
-        console.log("Album: " + data.tracks.items[3].album.name);
-    })
-    .catch(function (err) {
-        console.log("not working " + err);
-    });
-} 
-else if (process.argv[2] === "spotify-this-song") {
-    console.log("h¨")
-    music(userInput);
-} 
-else if (process.argv[2] === "movie-this" && process.argv[3] === undefined) {
-    console.log("this condition is not running");
-    axios.get("http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy").then(
-        function (response) {
-            // console.log(response);
-            console.log("Title of the movie: " + response.data.Title);
-            console.log("Released: " + response.data.Released);
-            console.log("IMDB Rating of the movie: " + response.data.imdbRating);
-            console.log("Rotten Tomatoes Rating of the movie " + response.data.Ratings[2]);//Figure it out
-            console.log("Country: " + response.data.Country);
-            console.log("Language: " + response.data.Language);
-            console.log("Plot of the movie: " + response.data.Plot);
-            console.log("Actors: " + response.data.Actors);
-        }).catch(function (err) {
-            console.log(err);
-    });;
-} 
-else if (process.argv[2] === "movie-this") {
-    movie(userInput);
-}
-else if (process.argv[2] === "concert-this") {
-    bands();
-}
+
+//----------------------------------------------------------
+//DO-WHAT-IT-SAYS
+
+fs.readFile("random.txt", "utf8", function(error, data) {
+    if (error) {
+      return console.log(error);
+    }
+    console.log(data);
+    var dataArr = data.trim().split(",");
+    // console.log(dataArr);
+    randomAction = dataArr[0].trim();
+    randomInput = dataArr[1].trim();
+
+    // console.log(randomAction);
+    // console.log(randomInput);
+  });
+
 
 //-------------------------------------------------
 //SPOTIFY
@@ -97,6 +77,7 @@ function movie() {
     });;
 }
 
+
 //----------------------------------------------------------
 //BANDS IN TOWN
 
@@ -118,10 +99,59 @@ function bands(){
 }
 
 
-// 4. `node liri.js do-what-it-says`
+//----------------------------------------------------------
+//USER INTERACTION
 
-//    * Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
+if (process.argv[2] | randomAction === "spotify-this-song" && process.argv[3] === undefined) {
+    console.log("hi¨")
+    spotify
+    .search({
+        type: 'track',
+        query: "the+sign"
+    })
+    .then(function (data) {
+        console.log("Artist: " + data.tracks.items[3].artists[0].name);
+        console.log("Song: " + data.tracks.items[3].name);
+        console.log("Link to the song: " + data.tracks.items[3].external_urls.spotify);
+        console.log("Album: " + data.tracks.items[3].album.name);
+    })
+    .catch(function (err) {
+        console.log("not working " + err);
+    });
+} 
+else if (process.argv[2] | randomAction === "spotify-this-song") {
+    console.log("h¨")
+    music(userInput | randomInput);
+} 
+else if (process.argv[2] === "movie-this" && process.argv[3] === undefined) {
+    console.log("this condition is not running");
+    axios.get("http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=trilogy").then(
+        function (response) {
+            // console.log(response);
+            console.log("Title of the movie: " + response.data.Title);
+            console.log("Released: " + response.data.Released);
+            console.log("IMDB Rating of the movie: " + response.data.imdbRating);
+            console.log("Rotten Tomatoes Rating of the movie " + response.data.Ratings[2]);//Figure it out
+            console.log("Country: " + response.data.Country);
+            console.log("Language: " + response.data.Language);
+            console.log("Plot of the movie: " + response.data.Plot);
+            console.log("Actors: " + response.data.Actors);
+        }).catch(function (err) {
+            console.log(err);
+    });;
+} 
+else if (process.argv[2] === "movie-this") {
+    movie(userInput);
+}
+else if (process.argv[2] === "concert-this") {
+    bands();
+}
 
-//      * It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
+
+
+
+
+
+
 
 //      * Edit the text in random.txt to test out the feature for movie-this and concert-this.
